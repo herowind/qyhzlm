b76249b2d0aad4054d4ca2b234d6358514d88c8f
Oliver Wang <whtaok@163.com>
2017-11-20 11:00:16 +0800

    333

--- domain_admin/public/index.php
+++ domain_admin/public/index.php
@@ -1,4 +1,4 @@
 <?php
-//ini_set('session.cookie_domain', '.dlmicroworld.com');
-// 加载框架引导文件
-require '../../framework/thinkphp/start.php';
+namespace think;
+require __DIR__ . '/../../framework/base.php';
+Container::get('app')->run()->send();
